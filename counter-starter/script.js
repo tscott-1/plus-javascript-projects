c0ba7609e0ae2e2ccc0eee518a29bec5bee541fd
Tenille <elmofreak@gmail.com>
2024-11-09 10:04:43 +0800

    completing counter project

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -1,8 +1,40 @@
-const add = () => {
+const add = (step) => {
     // We gave the number element an id of number so we can use that to get the element
     let numberElement = document.getElementById("number");
     // We can use the innerText property to get the text inside the element
     let number = numberElement.innerText;
-        number = parseInt(number) + 1;
+        number = parseInt(number) + step;
     	numberElement.innerText = number;
+  };
+
+
+const subtract = (step) => {
+    // We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+    // We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText;
+        number = parseInt(number) - step;
+    	numberElement.innerText = number;
+  };
+
+
+
+  const reset = () => {
+    // We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+    // We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText = 0;
+  };
+
+
+  const custom = () => {
+    // We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+    let customStep = document.getElementById("step");
+        // We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText;
+    let step = customStep.innerText;
+        // number = parseInt(number) + parseInt(step);
+        number = parseInt(step);
+        numberElement.innerText = number;
   };
