965a249483e5b74a57c815137bc6e026eec47662
Tenille <elmofreak@gmail.com>
2024-11-12 17:05:39 +0800

    working through to-do-list exercise

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,46 +1,79 @@
 let todoTasks = ["Walk Chilli", "Make Dinner"];
 let todoTasksStatus = [false, true];
+let todoImportantTask = [true,false];
 
 const todoList = document.getElementById("todo-list");
 
-for (const [index, task] of todoTasks.entries()) {
-  // Create a <p> element to store the task description
-  const newTodoTaskTextElement = document.createElement("p");
-  newTodoTaskTextElement.innerText = task;
-
-  // Apply a CSS class to the completed items
-  if (todoTasksStatus[index] == true) {
-    newTodoTaskTextElement.classList.add("complete");
-  }  
-  
-  // Create a <li> element to contain the paragraph
-  const newTodoTaskElement = document.createElement("li");
-  newTodoTaskElement.appendChild(newTodoTaskTextElement);
-  
-// Adding a button to mark each item as complete
-   const completeButtonElement = document.createElement("input");
-   completeButtonElement.type = "button";
-   completeButtonElement.value = "Completed";
-   newTodoTaskElement.appendChild(completeButtonElement);
-
-  // Add the <li> element to the list
-  todoList.appendChild(newTodoTaskElement);
-}
 
 
 
 const addTask = () => {
-
+  const newTask = document.getElementById("new-task-text");
+  if (newTask.value) {
+    todoTasks.push(newTask.value);
+    todoTasksStatus.push(false);
+    newTask.value = "";
+    updateTodoList();
+  }
 };
 
 const updateTodoList = () => {
-
+  todoList.innerHTML = "";
+  for (const [index, task] of todoTasks.entries()) {
+    const newTodoTaskElement = createNewTodoItemElement(task, index);
+    todoList.appendChild(newTodoTaskElement);
+  }
 };
 
 const createNewTodoItemElement = (task, index) => {
+// Create a <p> element to store the task description
+const newTodoTaskTextElement = document.createElement("p");
+newTodoTaskTextElement.innerText = task;
 
+// Apply a CSS class to the completed items
+if (todoTasksStatus[index] == true) {
+  newTodoTaskTextElement.classList.add("complete");
+}  
+
+// Create a <li> element to contain the paragraph
+const newTodoTaskElement = document.createElement("li");
+newTodoTaskElement.appendChild(newTodoTaskTextElement);
+
+// Adding a button to mark each item as complete
+const completeButtonElement = document.createElement("input");
+completeButtonElement.type = "button";
+completeButtonElement.value = "Completed";
+completeButtonElement.onclick = function () {
+toggleComplete(index);
+};
+// Adding a button to mark important items
+const completeButtonElement = document.createElement("input");
+completeButtonElement.type = "button";
+completeButtonElement.value = "!";
+completeButtonElement.onclick = function () {
+toggleComplete(index);
+};
+newTodoTaskElement.appendChild(completeButtonElement);
+return newTodoTaskElement;
 };
 
 const toggleComplete = (index) => {
+// If it is complete, set it to incomplete. 
+// If it is incomplete, set it to complete.
+if (todoTasksStatus[index] == false) {
+  todoTasksStatus[index] = true;
+} else {
+  todoTasksStatus[index] = false;
+}
+updateTodoList();
     
 };
+
+
+const clearList = () => {
+  todoList.innerHTML = "";
+};
+
+
+
+updateTodoList();
